9f7af16b1f5d0ddcc2082fc087c74e326d86ffab
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-06-03 22:58:42 -0400

    learning Java

---
+++ src/Addition.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number1;
+        int number2;
+        int sum;
+
+        System.out.print("Passe o primeiro numero para a soma: ");
+        number1 = input.nextInt();
+
+        System.out.print("Passe o segundo numero para a soma: ");
+        number2 = input.nextInt();
+
+        sum = number1 + number2;
+
+        System.out.printf("Sum is %d%n", sum);
+    }
+}
