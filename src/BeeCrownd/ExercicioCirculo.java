cc461857a31c2be4f6207ea64b021787f0d0ecea
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-06-14 00:46:02 -0400

    14/06/2025

---
+++ src/BeeCrownd/ExercicioCirculo.java
@@ -0,0 +1,24 @@
+package BeeCrownd;
+import java.util.Scanner;
+
+
+//A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+//
+//        - Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+//        Entrada
+//
+//A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+//Saída
+//Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+public class ExercicioCirculo {
+    public static void main(String[] args){
+    Scanner en = new Scanner(System.in);
+    double raio = en.nextDouble(); // Lê o raio sem imprimir nada antes
+
+    double pi = 3.14159; // Usando o π especificado no problema
+    double area = pi * raio * raio;
+
+        System.out.printf("A=%.4f%n", area); // Formata com 4 casas decimais
+    }
+}
